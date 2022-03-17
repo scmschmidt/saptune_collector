619e2c374e8b01aad7c9938459eaf428b1281ea9
Sören Schmidt <sschmidt@suse.com>
2022-03-17 09:37:23 +0100

    First commit with v0.5

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
 # saptune_collector
-Retrieve saptune data and present them as metrics for prometheus node_exporter's textfile collector
+
+Retrieve saptune data and present them as metrics for prometheus node_exporter's textfile collector.
+
+:exclamation: This repo is to present code snippets for ((BLOG LINK)) only. It is not meant as a growing maintained project. Use the code as it is and adapt it to your needs. 
+
+
+## The collector script: `saptune_collector.sh`
+
+The script executes various saptune checks and writes a prometheus metric description to stdout.
+
+Contrary to the saptune-exporter (https://github.com/SUSE/saptune_exporter), the script collects
+different information and uses different metric names:
+
+| Name                            | Type  | Description | Example | 
+| ---                             | ---   | --- | --- | 
+| `sc_timestamp`                  | gauge | Timestamp (epoch) when metrics were generated. | `sc_timestamp 1646841231` |
+| `sc_saptune`                    | gauge | Contains saptune version inforamtion (configured and package). | `sc_saptune{version="3",package="saptune-3.0.2-8.22.2.x86_64"} 1` |
+| `sc_saptune_note_enabled`       | gauge | Lists all available Notes and if they're enabled by a solution (1), enabled manually (2), not enabled at all or reverted (0)." | `saptune_note_enabled{note_desc="Linux: User and system resource limits",note_id="1771258"} 1` |
+| `sc_saptune_note_applied`       | gauge | Lists all available Notes and if they're applied (1) or not (0). | `saptune_note_applied{note_desc="Linux: User and system resource limits",note_id="1771258"} 1` |
+| `sc_saptune_solution_enabled`   | gauge | Lists all available Solutions and if it is enabled (1) or not (0). | `sc_saptune_solution_enabled{solution_name="HANA"} 1` |
+| `sc_saptune_solution_applied`   | gauge | Lists all available Solutions and if it is applied (1) or not (0).  | `sc_saptune_solution_applied{solution_name="HANA"} 1` |
+| `sc_saptune_compliance` | gauge | Shows if applied Notes are compliant (1) or not (0). | `sc_saptune_compliance 1`
+
+ 
+### Usage with the prometheus textfile collector
+
+To be used by the textfile collector of the prometheus node exporter, the output needs to put into a file which should be updated regularly.
+This can be achieved by a systemd timer. 
+
+### `saptune_collector.service`
+
+```
+[Unit]
+Description=Collects saptune metrics for prometheus textfile collector.
+ 
+[Service]
+Type=oneshot
+ExecStartPre=/usr/bin/rm -f <path_to_data_file>  
+ExecStart=/bin/sh -c 'exec <path_to_saptune_collector.sh > <path_to_data_file>.tmp'
+ExecStartPost=/usr/bin/mv <path_to_data_file>.tmp <path_to_data_file>
+```
+
+### `saptune_collector_timer`
+
+```
+[Unit]
+Description=Periodic collection of saptune metrics for prometheus textfile collector.
+
+[Timer]
+Unit=saptune_collector.service
+OnCalendar=*:0/15
+RandomizedDelaySec=10
+
+[Install]
+WantedBy=timers.target
+```
+
+## Installing
+                                                              
+1. Put `saptune_collector.sh` to `/usr/local/sbin/`.
+                                                              
+2. Create `saptune_collector.service` and `saptune_collector_timer` in `/etc/systemd/system/`.
+
+3. Add the following argument to the line `ARGS=` in `/etc/sysconfig/prometheus-node_exporter`: `--collector.textfile.directory=/opt/prometheus/data/`
+
+4. Execute: 
+ 
+   ``` 
+   ~ # systemctl daemon-reload 
+   ~ # systemctl restart prometheus-node_exporter 
+   ~ # systemctl enable --now saptune_collector.timer 
+   ``` 
---
+++ saptune_collector.sh
@@ -0,0 +1,186 @@
+#!/bin/bash
+#
+# ------------------------------------------------------------------------------
+# Copyright (c) 2019 SUSE LLC
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of version 3 of the GNU General Public License as published by the
+# Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, contact SUSE Linux GmbH.
+#
+# ------------------------------------------------------------------------------
+# Author: Sören Schmidt <soeren.schmidt@suse.com>
+#
+# saptune_collector 
+#
+# Collects data about the saptune configuration on this host and writes them to stdout as a prometheus metrics.
+# This collector only supports saptune v3 for now. 
+#
+# Changelog:    28.02.2022  v0.1     first incarnation
+#               09.03.2022  v0.2     heavy rework
+#               11.03.2022  v0.3     some bug fixing
+#               11.03.2022  v0.4     added GPL header and saptune version metric
+#               16.03.2022  v0.5     fixed issue with list output ('Remember...' lines)
+#                                    sc_saptune{version=...} also set, if package is != v3
+#                                    sc_timestamp now printed even with reduced metric
+#                                    added information about how Note was enabled/applied
+#               16.03.2022  v0.5.1   get rid of typos...
+#
+# Exit codes:
+#
+#   0   everything is fine 
+#   1   the saptune binary is not present
+#   2   saptune version is not supported
+#   3   saptune data could not be collected
+#
+
+version="0.5.1"
+
+# Define exit codes.
+exit_ok=0
+exit_no_saptune=1
+exit_unsupported=2
+exit_retrieval_error=3
+
+# Path to the saptune executable.
+saptune_exe='/usr/sbin/saptune'
+
+# ---- functions ----
+
+function print_time_and_version_metric() {
+    # Print the timestamp and version metric on stdout.
+    #
+    # Param:  version, package
+    # Return: -
+
+    local saptune_presence=1
+
+    # If version information is missing, we assume saptune is missing.
+    [ -z "${1}" -o -z "${2}" ] && saptune_presence=0
+
+    echo "# HELP sc_timestamp Timestamp (epoch) when metrics were generated."
+    echo "# TPYE sc_timestamp counter"
+    echo "sc_timestamp $(date +'%s')"
+    echo 
+    echo "# HELP sc_saptune Version information of saptune."
+    echo "# TPYE sc_saptune gauge"
+    echo "sc_saptune{version=\"${1}\",package=\"${2}\"} ${saptune_presence}"
+    echo 
+}
+
+# ---- main ----
+
+# Terminate if there is no saptune executable.
+[ -e "${saptune_exe}" ] || 
+    { print_time_and_version_metric "" "" ; exit ${exit_no_saptune} ; }
+
+# Get saptune package version.
+saptune_package=$(rpm -q saptune) || exit ${exit_retrieval_error}
+saptune_package_version="${saptune_package#saptune-}" ; saptune_package_version="${saptune_package_version%%.*}"
+
+# Check for configured version.
+saptune_version=$("${saptune_exe}" version) || exit ${exit_retrieval_error}
+saptune_version="${saptune_version: -2:1}"  # Works only with one digit version numbers enclosed by ' at the end of the output!
+
+# Exit in case it is not v3.
+[ "${saptune_package_version}" != '3' -o "${saptune_version}" != '3'  ] && 
+    { print_time_and_version_metric "${saptune_version}" "${saptune_package}" ; exit ${exit_unsupported} ; }
+
+# Retrieve all available Notes (available_notes) and there status (note_status) and Solutions (available_solutions).
+declare -A available_notes available_solutions note_status
+shopt -s extglob
+while read line ; do 
+    line="${line#*:}"
+    status="${line:1:6}"
+    line="${line:7}"
+    id="${line%% *}"   
+    name="${line:${#id}}" ; name="${name##*( )}"
+    available_notes[${id}]=${name}
+    case "${status}" in 
+        *\**)   note_status[${id}]=1 ;;  # 1 -> enabled by solution
+        *\+*)   note_status[${id}]=2 ;;  # 2 -> enabled manually
+        *\-*)   note_status[${id}]=0 ;;  # 0 -> reverted from solution
+        *)      note_status[${id}]=0 ;;  # 0 == not enabled
+    esac
+done < <(saptune note list |  sed '/^Remember/,$d' | grep -v -e 'current order of enabled note' -e '^[[:space:]]*Version [0-9]* from' -e '^All notes (+ denotes manually enabled' -e '^$' | expand -t 7 | nl -s :)
+while read line ; do 
+    id="${line%% - *}" ; id="${id%%*( )}"  
+    name="${line##* - }"   
+    available_solutions[${id}]=${name}
+done < <(saptune solution list |  sed '/^Remember/,$d' | grep -v -e '^All solutions' -e '^$' | expand -t 7 | cut -c 8- )
+
+# Retrieve enabled Notes.
+notes=$(saptune note enabled) || exit ${exit_retrieval_error}
+declare -A saptune_enabled_notes
+for note in ${notes} ; do 
+    saptune_enabled_notes[${note}]=1
+done
+
+# Retrieve applied Notes.
+notes=$(saptune note applied) || exit ${exit_retrieval_error}
+declare -A saptune_applied_notes
+for note in ${notes} ; do 
+    saptune_applied_notes[${note}]=1
+done
+
+# Retrieve enabled Solution.
+saptune_enabled_solution=$(saptune solution enabled | tr -d '\n') || exit ${exit_retrieval_error}
+
+# Retrieve applied Solution.
+saptune_applied_solution=$(saptune solution applied | tr -d '\n') || exit ${exit_retrieval_error}
+
+# Check for compliance.
+saptune_compliance=0
+saptune note verify >/dev/null 2>&1 && saptune_compliance=1
+
+# Output of full metrics.
+
+print_time_and_version_metric "${saptune_version}" "${saptune_package}"
+echo "# HELP sc_saptune_note_enabled Lists all available Notes and if they're enabled by a solution (1), enabled manually (2), not enabled at all or reverted (0)."
+echo "# TYPE sc_saptune_note_enabled gauge"
+for id in "${!available_notes[@]}"; do 
+    echo "sc_saptune_note_enabled{note_desc=\"${available_notes[$id]}\",note_id=\"${id}\"} ${note_status[$id]}"
+done
+echo 
+echo "# HELP sc_saptune_note_applied Lists all available Notes and if they're applied (1) or not (0)."
+echo "# TYPE sc_saptune_note_applied gauge"
+for id in "${!available_notes[@]}"; do 
+    status=0
+    [ -n "${saptune_applied_notes[${id}]}" ] && status=1
+    echo "sc_saptune_note_applied{note_desc=\"${available_notes[$id]}\",note_id=\"${id}\"} ${status}"
+done
+echo 
+echo "# HELP sc_saptune_solution_enabled Lists all available Solutions and if it is enabled (1) or not (0)."
+echo "# TYPE sc_saptune_solution_enabled gauge"
+for id in "${!available_solutions[@]}"; do 
+    status=0
+    [ "${id}" = "${saptune_enabled_solution}" ] && status=1
+    echo "sc_saptune_solution_enabled{note_list=\"${available_solutions[$id]}\",solution_id=\"${id}\"} ${status}"
+done
+echo 
+echo "# HELP sc_saptune_solution_applied Lists all available Solutions and if it is applied (1) or not (0)."
+echo "# TYPE sc_saptune_solution_applied gauge"
+for id in "${!available_solutions[@]}"; do 
+    status=0
+    [ "${id}" = "${saptune_applied_solution}" ] && status=1
+    echo "sc_saptune_solution_applied{note_list=\"${available_solutions[$id]}\",solution_id=\"${id}\"} ${status}"
+done
+echo 
+echo "# HELP sc_saptune_compliance Shows if applied Notes are compliant (1) or not (0)."
+echo "# TYPE sc_saptune_compliance gauge"
+echo "sc_saptune_compliance ${saptune_compliance}"
+
+# Bye.
+exit ${exit_ok}
+
+
+
+
+
+
